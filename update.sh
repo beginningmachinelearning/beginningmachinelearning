9cd66ea0531a60b4014514d5d17219e7874a9e96
Pat <pat@tildeteam.org>
2020-09-07 22:42:11 +0000

    stand up automation for angular updates

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /home/pat/src/beginningmachinelearning;
+date > README.txt;
+time yarn >> README.txt;
+time yarn run ng update >> README.txt;
+time git add README.txt;
+time git commit -m "start check update angular" -m "just checking";
+time yarn run ng update @angular/core @angular/cli rxjs;
+time yarn run ng update;
+echo "please stand by..." >> README.txt
+date >> README.txt;
+time git add README.txt;
+time git commit -m "end check update angular" -m "just checking";
+# time yarn run ng build --prod;
+# time yarn run ng test --watch=false --codeCoverage=true;
+time git add .;
+time git commit -m "update angular" -m "angular {cli, cdk, material}, and rxjs";
+time git push origin --all;
+
