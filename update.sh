f1b72f47a07aa3c2eb32b4a0047f74f9df4fb8d1
Pat <pat@tildeteam.org>
2020-09-21 18:27:51 +0000

    update update
    time git add .;

--- update.sh
+++ update.sh
@@ -6,8 +6,10 @@ time yarn >> README.txt;
 time yarn run ng update >> README.txt;
 time git add README.txt;
 time git commit -m "start check update angular" -m "just checking";
-time yarn run ng update @angular/core @angular/cli rxjs;
-time yarn run ng update;
+time ng update @angular/core @angular/cli rxjs;
+time git add .;
+time git commit -m "update angular" -m "angular {cli, cdk, material}, and rxjs";
+time ng update;
 echo "please stand by..." >> README.txt
 date >> README.txt;
 time git add README.txt;
